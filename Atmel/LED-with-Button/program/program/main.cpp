9b75ef1626554f520f6f6f273bdfbc056e5136f7
ThashmikaX <74942602+ThashmikaX@users.noreply.github.com>
2025-01-20 15:30:29 +0530

    Add initial files and code for LED with Button project

--- Atmel/LED-with-Button/program/program/main.cpp
+++ Atmel/LED-with-Button/program/program/main.cpp
@@ -3,17 +3,22 @@
 
 int main(void)
 {
-	/* Replace with your application code */
-	DDRD &= 0B01111111; // Port D's pin7 is set to input
-	DDRB |= 0800010000; //POrt B's pin4 is set to output
+	/* Configure PORTD pin 3 as input */
+	DDRD &= 0b11110111; // Clear bit 3 of DDRD to make it input (1 -> 0)
+	
+	/* Configure PORTB pin 5 as output (Arduino pin 13) */
+	DDRB |= 0b00100000; // Set bit 5 of DDRB to make it output (0 -> 1)
+
 	while (1)
 	{
-		if (PIND & OB10000000)
+		/* Check if button connected to pin 3 is pressed (logic high) */
+		if (PIND & 0b00001000) // Check bit 3 of PIND
 		{
-			PORTB=0B00010000; //make the port B's pin4 to high
-		}else
+			PORTB |= 0b00100000; // Set bit 5 of PORTB to make LED on
+		}
+		else
 		{
-			PORTB &= 0B11101111;//make the port B's pin4 to low
+			PORTB &= 0b11011111; // Clear bit 5 of PORTB to make LED off
 		}
 	}
 }
